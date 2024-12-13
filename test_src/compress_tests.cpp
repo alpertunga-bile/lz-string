373bb884b949863dd16b04a021da1911f102b4b2
alpertunga-bile <holy_celestine@hotmail.com>
2024-12-13 15:21:21 +0300

    new compress tests are added

--- test_src/compress_tests.cpp
+++ test_src/compress_tests.cpp
@@ -2,68 +2,21 @@
 
 #include <gtest/gtest.h>
 
-TEST(Compress, HelloWorld) {
-  std::string input(hello_world);
+constexpr LZStringOptions compression = LZStringOptions::INVALID_UTF16;
 
-  std::vector<uint16_t> result = {1157,  12342, 24822, 64,   60048,
-                                  10032, 1242,  1056,  53248};
+#define MAKE_COMPRESS_TEST(test_name, test_data_name)             \
+  TEST(Compress, test_name) {                                     \
+    auto [input, result] =                                        \
+        get_compress_test_variables(test_data_name, compression); \
+                                                                  \
+    EXPECT_TRUE(compare_u16(compression, input, result));         \
+  }
 
-  EXPECT_TRUE(compare_u16(LZStringOptions::INVALID_UTF16, input, result));
-}
-
-TEST(Compres, AllASCII) {
-  std::string input(all_ascii);
-
-  std::vector<uint16_t> result = {
-      260,   8260,  3136,  9226,  16484, 1824,  1281,  10245, 16490, 208,
-      5760,  29699, 53248, 49187, 76,    816,   704,   11008, 27651, 45057,
-      49191, 92,    880,   960,   12032, 31745, 63488, 2052,  4100,  8216,
-      16392, 32849, 98,    452,   72,    1168,  1312,  6720,  3200,  22784,
-      29185, 58368, 10244, 20484, 40985, 16394, 32853, 106,   468,   104,
-      1232,  1440,  6976,  3712,  23808, 31232, 64000, 1536,  34304, 17920,
-      50688, 9728,  42496, 26112, 58880, 5632,  38400, 22016, 54784, 13824,
-      46592, 30208, 62976, 3584,  36352, 19968, 52736, 11776, 44544, 28160,
-      60928, 7680,  40448, 24064, 56832, 15872, 48640, 32320, 0};
-
-  EXPECT_TRUE(compare_u16(LZStringOptions::INVALID_UTF16, input, result));
-}
-
-TEST(Compress, JSON) {
-  std::string input(temp_json);
-
-  std::vector<uint16_t> result = {
-      14210, 8398,  736,   19977, 24694, 1841,  184,   16393, 664,   1739,
-      1968,  3328,  1027,  43749, 5632,  38920, 16958, 8640,  11136, 46593,
-      6245,  2562,  12292, 49203, 7469,  29410, 49536, 8604,  5328,  6243,
-      43207, 2617,  22762, 35461, 5184,  20244, 437,   15334, 61343, 32790,
-      31745, 22530, 59861, 49856, 5510,  198,   4178,  33674, 10630, 5152,
-      330,   57345, 26384, 32776, 44603, 45698, 15282, 12032, 805,   40349,
-      33536, 936,   33804, 11780, 43520, 12974, 25,    33792, 15242, 34336,
-      17920, 7094,  11836, 31496, 47,    44166, 9222,  1280,  16021, 44419,
-      37768, 10373, 1300,  7512,  15204, 3910,  9130,  3887,  8448,  11432,
-      48161, 14947, 40221, 35844, 9898,  32,    39994, 33838, 11844, 40228,
-      4728,  18432, 12426, 60680, 23,    35900, 18344, 57737, 46413, 18282,
-      34483, 22131, 19901, 33823, 29206, 45995, 33255, 47248, 42001, 2164,
-      49704, 28169, 34475, 62320, 30720, 16982, 6547,  4451,  58682, 35009,
-      51267, 9290,  42594, 43264};
-
-  EXPECT_TRUE(compare_u16(LZStringOptions::INVALID_UTF16, input, result));
-}
-
-TEST(Compress, JSONFloat) {
-  std::string input(temp_json_float);
-
-  std::vector<uint16_t> result = {
-      14210, 8294,  864,   62984, 24622, 8215,  408,   1856,  17920, 45984,
-      44036, 57350, 33792, 19616, 5568,  8968,  1330,  16422, 21511, 26432,
-      3642,  23072, 15210, 1847,  32843, 110,   54273, 35655, 33920, 11520,
-      1548,  30098, 25028, 23168, 3840,  3665,  47731, 36806, 1073,  45074,
-      9629,  13562, 31044, 24578, 29363, 39,    32889, 12295, 57350, 29248,
-      7005,  1538,  3304,  4934,  16420, 50500, 49185, 50497, 49157, 53794,
-      32928, 694,   40966, 13184, 1879,  58240, 177,   29575, 29765, 855,
-      57923, 17093, 50209, 1444,  16486, 25441, 24581, 62182, 42355, 34470,
-      8304,  20043, 18638, 53013, 51776, 10887, 59095, 59649, 21062, 63298,
-      9143,  42307, 41903, 39424};
-
-  EXPECT_TRUE(compare_u16(LZStringOptions::INVALID_UTF16, input, result));
-}
+MAKE_COMPRESS_TEST(AllASCII, "all_ascii")
+MAKE_COMPRESS_TEST(HelloWorld, "hello_world")
+MAKE_COMPRESS_TEST(LoremIpsum, "lorem_ipsum")
+MAKE_COMPRESS_TEST(PI, "pi")
+MAKE_COMPRESS_TEST(Repeated, "repeated")
+MAKE_COMPRESS_TEST(Tattoo, "tattoo")
+MAKE_COMPRESS_TEST(JSON, "temp_json")
+MAKE_COMPRESS_TEST(JSONFloat, "temp_json_float")
